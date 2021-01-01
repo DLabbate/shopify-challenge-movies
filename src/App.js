d2c4ddf47e8a33833413e758ed7a0a6e5b77a25c
Domenic Labbate <domeniclabbate@gmail.com>
2020-12-31 19:35:44 -0500

    Removed hardcoded data

--- src/App.js
+++ src/App.js
@@ -1,99 +1,18 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import MovieItem from "./components/MovieItem";
 import SearchBar from "./components/SearchBar";
 
-const movieList = [
-  {
-    Title: "The Lord of the Rings: The Fellowship of the Ring",
-    Year: "2001",
-    imdbID: "tt0120737",
-    Type: "movie",
-    Poster:
-      "https://m.media-amazon.com/images/M/MV5BN2EyZjM3NzUtNWUzMi00MTgxLWI0NTctMzY4M2VlOTdjZWRiXkEyXkFqcGdeQXVyNDUzOTQ5MjY@._V1_SX300.jpg",
-  },
-  {
-    Title: "The Lord of the Rings: The Return of the King",
-    Year: "2003",
-    imdbID: "tt0167260",
-    Type: "movie",
-    Poster:
-      "https://m.media-amazon.com/images/M/MV5BNzA5ZDNlZWMtM2NhNS00NDJjLTk4NDItYTRmY2EwMWZlMTY3XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg",
-  },
-  {
-    Title: "The Lord of the Rings: The Two Towers",
-    Year: "2002",
-    imdbID: "tt0167261",
-    Type: "movie",
-    Poster:
-      "https://m.media-amazon.com/images/M/MV5BZGMxZTdjZmYtMmE2Ni00ZTdkLWI5NTgtNjlmMjBiNzU2MmI5XkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg",
-  },
-  {
-    Title: "The Lord of the Rings",
-    Year: "1978",
-    imdbID: "tt0077869",
-    Type: "movie",
-    Poster:
-      "https://m.media-amazon.com/images/M/MV5BOGMyNWJhZmYtNGQxYi00Y2ZjLWJmNjktNTgzZWJjOTg4YjM3L2ltYWdlXkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_SX300.jpg",
-  },
-  {
-    Title: "The Making of 'The Lord of the Rings'",
-    Year: "2002",
-    imdbID: "tt0302751",
-    Type: "movie",
-    Poster:
-      "https://m.media-amazon.com/images/M/MV5BMmE3MzQwYjYtODFmOS00M2MxLWJjYTUtZTNjYjhmYjg4MzU2XkEyXkFqcGdeQXVyMTEyMDcwNw@@._V1_SX300.jpg",
-  },
-  {
-    Title: "A Passage to Middle-earth: The Making of 'Lord of the Rings'",
-    Year: "2001",
-    imdbID: "tt0301246",
-    Type: "movie",
-    Poster:
-      "https://m.media-amazon.com/images/M/MV5BOTc3NjA5MDYxNF5BMl5BanBnXkFtZTcwMTY4MzEyMQ@@._V1_SX300.jpg",
-  },
-  {
-    Title:
-      "National Geographic: Beyond the Movie - The Lord of the Rings: Return of the King",
-    Year: "2003",
-    imdbID: "tt0405185",
-    Type: "movie",
-    Poster:
-      "https://m.media-amazon.com/images/M/MV5BZDBjNzI4MTItMWUyMy00MTU4LWI0Y2EtYTdkNjg5NmJkNjgxXkEyXkFqcGdeQXVyNTc0NjY1ODk@._V1_SX300.jpg",
-  },
-  {
-    Title:
-      "Master of the Rings: The Unauthorized Story Behind J.R.R. Tolkien's 'Lord of the Rings'",
-    Year: "2001",
-    imdbID: "tt0308561",
-    Type: "movie",
-    Poster:
-      "https://m.media-amazon.com/images/M/MV5BMTI1ODc4OTEwOF5BMl5BanBnXkFtZTcwNzA0NTc0MQ@@._V1_SX300.jpg",
-  },
-  {
-    Title:
-      "Creating the Lord of the Rings Symphony: A Composer's Journey Through Middle-Earth",
-    Year: "2004",
-    imdbID: "tt0456052",
-    Type: "movie",
-    Poster:
-      "https://m.media-amazon.com/images/M/MV5BNTViY2MyOTktNTEyYi00MjAwLWE4YWQtNDhmOTlkZTE2MzQ5XkEyXkFqcGdeQXVyNjU2MzA0OTE@._V1_SX300.jpg",
-  },
-  {
-    Title: "The Lord of the Rings: The Quest Fulfilled",
-    Year: "2003",
-    imdbID: "tt0382814",
-    Type: "movie",
-    Poster: "N/A",
-  },
-];
-
 function App() {
+  const [searchList, setSearchList] = useState([]); //List of movies resulting from the search
+  const [searchValue, setSearchValue] = useState(""); //Text input of the search bar. e.g "The Lord of the Rings"
+
   const getMovies = (search) => {
     fetch(`http://www.omdbapi.com/?s=${search}&apikey=45ae6804&type=movie`)
       .then((res) => res.json())
       .then((result) => {
         console.log(result);
+        setSearchList(result.Search);
       });
   };
 
@@ -108,7 +27,7 @@ function App() {
       </div>
       <SearchBar />
       <div className="searchList">
-        {movieList.map((movie) => {
+        {searchList.map((movie) => {
           return <MovieItem {...movie} key={movie.imdbID} />;
         })}
       </div>
